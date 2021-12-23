e8914759984e00d88f96b665a3782c84235be3be
BrandonOng22 <brandonong22@gmail.com>
2021-12-23 04:34:15 -0600

    Starting flash project

---
+++ verilog/rtl/flash_array_8x8.v
@@ -0,0 +1,31 @@
+
+`default_nettype none
+
+module flash_array_8x8 (
+`ifdef USE_POWER_PINS
+    inout vccd1, vssd1,
+`endif
+
+    // BL
+    inout [7:0] BL,
+
+    // Select Lines
+    input [1:0] SSL,
+    input [1:0] GSL,
+
+    // WL
+    input [3:0] WL0,
+    input [3:0] WL1,
+
+    // SL
+    input SL,
+    input PSUB,
+
+    // senseamp
+    input sen1,
+    input sen2,
+    input [3:0] out_en,
+    output [7:0] out
+);
+endmodule
+`default_nettype wire
--- verilog/rtl/uprj_netlists.v
+++ verilog/rtl/uprj_netlists.v
@@ -17,12 +17,12 @@
 `include "defines.v"
 `define USE_POWER_PINS
 
-`ifdef GL
-    // Assume default net type to be wire because GL netlists don't have the wire definitions
-    `default_nettype wire
-    `include "gl/user_project_wrapper.v"
-    `include "gl/user_proj_example.v"
-`else
-    `include "user_project_wrapper.v"
-    `include "user_proj_example.v"
-`endif
+// `ifdef GL
+//     // Assume default net type to be wire because GL netlists don't have the wire definitions
+//     `default_nettype wire
+//     `include "gl/user_project_wrapper.v"
+//     `include "gl/user_proj_example.v"
+// `else
+`include "user_project_wrapper.v"
+`include "flash_array_8x8.v"
+// `endif
